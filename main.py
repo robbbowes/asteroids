a3ac3d09e3ef730fb29fbd62a293091377ef5b11
https://github.com/robbbowes <robertjamesbowes@gmail.com>
2025-12-06 17:25:59 +0100

    Add overpowered shot

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -18,12 +19,16 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
 
-    asteroids = pygame.sprite.Group()
     Asteroid.containers = (asteroids, updatable, drawable)
 
     AsteroidField.containers = (updatable)
+
+    Shot.containers = (shots, updatable, drawable)
 
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
---
+++ shot.py
@@ -0,0 +1,13 @@
+import pygame
+from circleshape import CircleShape
+from constants import SHOT_RADIUS
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "yellow", self.position, SHOT_RADIUS)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
